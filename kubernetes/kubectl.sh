42f31deabd29e5e7a3bfd83711f9ff84f24b0cb5
franklin <kevin.kim@bestbytes.com>
2023-01-16 09:48:11 +0100

    feat: add kubectl

---
+++ kubernetes/kubectl.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# vars
+os="${os:-${1}}"
+arch="${arch:-${2}}"
+version="${version:-${3}}"
+
+# download
+curl -fL "https://dl.k8s.io/release/v${version}/bin/${os}/${arch}/kubectl" -o "${BIN_DIR}/kubectl-${version}-${os}-${arch}"
+
+# extract
+chmod a+x "${BIN_DIR}/kubectl-${version}-${os}-${arch}"

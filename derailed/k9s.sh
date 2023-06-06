81038b19a8a985c8a89ad0516fd52d26deb279d6
franklin <kevin.kim@bestbytes.com>
2023-06-06 10:26:29 +0200

    wip: debug

--- derailed/k9s.sh
+++ derailed/k9s.sh
@@ -25,16 +25,20 @@ arm64) arch_alias="arm64";;
 esac
 
 # download
+echo "downloading... https://github.com/derailed/k9s/releases/download/v${version}/k9s_${os_alias}_${arch_alias}.tar.gz"
 curl -fL "https://github.com/derailed/k9s/releases/download/v${version}/k9s_${os_alias}_${arch_alias}.tar.gz" -o "${TEMP_DIR}/k9s.tar.gz"
 curl -fL "https://github.com/derailed/k9s/releases/download/v${version}/checksums.txt" -o "${TEMP_DIR}/k9s.tar.gz.sha256"
 
 # validate
+echo "validating..."
 echo "$(cat ${TEMP_DIR}/k9s.tar.gz.sha256 | grep "k9s_${os_alias}_${arch_alias}.tar.gz" | awk '{print $1;}')  ${TEMP_DIR}/k9s.tar.gz" | shasum -a 256 --check --quiet
 
 # extract
+echo "extracting..."
 tar -xzvf "${TEMP_DIR}/k9s.tar.gz" -C "${TEMP_DIR}" k9s
 mv "${TEMP_DIR}/k9s" "${BIN_DIR}/k9s-${version}-${os}-${arch}"
 chmod a+x "${BIN_DIR}/k9s-${version}-${os}-${arch}"
 
 # cleanup
+echo "cleaning up..."
 rm "${TEMP_DIR}/k9s.tar.gz" "${TEMP_DIR}/k9s.tar.gz.sha256"

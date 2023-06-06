e78775a28abe974b077a2232d728db1834fc0b1c
franklin <kevin.kim@bestbytes.com>
2023-06-06 11:29:55 +0200

    wip: update script

--- derailed/k9s.sh
+++ derailed/k9s.sh
@@ -14,15 +14,16 @@ esac
 
 case $arch in
 amd64)
-  if (( $( semver_compare "${version}" "0.27.0" ) >= 0 ))
+  if semver_compare "${version}" "0.27.0"
   then
-    arch_alias="amd64"
-  else
     arch_alias="x86_64"
+  else
+    arch_alias="amd64"
   fi
   ;;
 arm64) arch_alias="arm64";;
 esac
+exit
 
 # download
 echo "downloading... https://github.com/derailed/k9s/releases/download/v${version}/k9s_${os_alias}_${arch_alias}.tar.gz"
@@ -30,15 +31,12 @@ curl -fL "https://github.com/derailed/k9s/releases/download/v${version}/k9s_${os
 curl -fL "https://github.com/derailed/k9s/releases/download/v${version}/checksums.txt" -o "${TEMP_DIR}/k9s.tar.gz.sha256"
 
 # validate
-echo "validating..."
 echo "$(cat ${TEMP_DIR}/k9s.tar.gz.sha256 | grep "k9s_${os_alias}_${arch_alias}.tar.gz" | awk '{print $1;}')  ${TEMP_DIR}/k9s.tar.gz" | shasum -a 256 --check --quiet
 
 # extract
-echo "extracting..."
 tar -xzvf "${TEMP_DIR}/k9s.tar.gz" -C "${TEMP_DIR}" k9s
 mv "${TEMP_DIR}/k9s" "${BIN_DIR}/k9s-${version}-${os}-${arch}"
 chmod a+x "${BIN_DIR}/k9s-${version}-${os}-${arch}"
 
 # cleanup
-echo "cleaning up..."
 rm "${TEMP_DIR}/k9s.tar.gz" "${TEMP_DIR}/k9s.tar.gz.sha256"

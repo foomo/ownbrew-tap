f2283e7d77c5429ee29d59dcd599cd146cdf4e79
franklin <kevin.kim@bestbytes.com>
2023-07-31 17:51:19 +0200

    fix: arch

--- derailed/k9s.sh
+++ derailed/k9s.sh
@@ -13,11 +13,11 @@ darwin) os_alias="Darwin";;
 esac
 
 echo "download ..."
-curl -fL "https://github.com/derailed/k9s/releases/download/v${version}/k9s_${os_alias}_${arch_alias}.tar.gz" -o "${TEMP_DIR}/k9s.tar.gz"
+curl -fL "https://github.com/derailed/k9s/releases/download/v${version}/k9s_${os_alias}_${arch}.tar.gz" -o "${TEMP_DIR}/k9s.tar.gz"
 curl -fL "https://github.com/derailed/k9s/releases/download/v${version}/checksums.txt" -o "${TEMP_DIR}/k9s.tar.gz.sha256"
 
 echo "validate ..."
-echo "$(cat ${TEMP_DIR}/k9s.tar.gz.sha256 | grep "k9s_${os_alias}_${arch_alias}.tar.gz" | awk '{print $1;}')  ${TEMP_DIR}/k9s.tar.gz" | shasum -a 256 --check --quiet
+echo "$(cat ${TEMP_DIR}/k9s.tar.gz.sha256 | grep "k9s_${os_alias}_${arch}.tar.gz" | awk '{print $1;}')  ${TEMP_DIR}/k9s.tar.gz" | shasum -a 256 --check --quiet
 
 echo "extract ..."
 tar -xzvf "${TEMP_DIR}/k9s.tar.gz" -C "${TEMP_DIR}" k9s

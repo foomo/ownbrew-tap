8893469edff18b603fe75f07a3635a7532ab7f7f
franklin <kevin.kim@bestbytes.com>
2023-08-18 07:22:35 +0200

    fix: remove include

--- accurics/terrascan.sh
+++ accurics/terrascan.sh
@@ -2,7 +2,29 @@
 
 set -e
 
-source .include/log.sh
+# colors
+CRed='\033[1;31m'
+CGray='\033[0;37m'
+CGreen='\033[1;32m'
+CYellow='\033[1;33m'
+NC='\033[0m'
+
+# logging
+info() {
+  echo -e "${CGray}${1}${NC}"
+}
+
+warn() {
+  echo -e "${CYellow}${1}${NC}"
+}
+
+error() {
+  echo -e "${CRed}${1}${NC}"
+}
+
+success() {
+  echo -e "${CGreen}${1}${NC}"
+}
 
 echo "DEPRECATED: moved to tenable/terrascan"
 
